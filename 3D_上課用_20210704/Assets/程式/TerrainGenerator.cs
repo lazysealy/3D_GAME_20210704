dea522f221e2513185b6610bf13f149587d46d2a
lazysealy <82298507+lazysealy@users.noreply.github.com>
2021-07-04 17:17:28 +0800

    20210704_進度完整

--- 3D_上課用_20210704/Assets/程式/TerrainGenerator.cs
+++ 3D_上課用_20210704/Assets/程式/TerrainGenerator.cs
@@ -3,10 +3,22 @@
 public class TerrainGenerator : MonoBehaviour
 {
     #region 欄位
-    [Header("地形大小")]
+    /// <summary>
+    /// 長
+    /// </summary>
+    [Header("長")]
     public int x;
-    public int y;
-
+    /// <summary>
+    /// 寬
+    /// </summary>
+    [Header("寬")]
+    public int z;
+    [Header("地形細節 : 坡度的起伏"), Tooltip("數字越大起伏越小 , 數字 1 為沒有起伏"), Range(1, 100)]
+    public float detial;
+    [Header("地形高度")]
+    public float height;
+    [Header("地形物件的高度")]
+    public float heightTerrain = 0.8f;
     // 陳列
     // 語法 : 類型後加上中括號［］
     // 用途 : 存放相同類型的多筆資料
@@ -16,24 +28,80 @@ public class TerrainGenerator : MonoBehaviour
     /// </summary>
     [Header("地形物件 : 0 草地, 1 泥土, 2 石頭")]
     public GameObject[] objTerrains;
+    [Header("玩家")]
+    public Transform traPlayer;
 
     /// <summary>
     /// 地形群組 : 用來放生成的地形物件
     /// </summary>
     private Transform traTerrainGroup;
+    /// <summary>
+    /// 隨機地形數值
+    /// </summary>
+    private int randomTerrain;
+
     #endregion
 
     #region 事件
     private void Start()
     {
         traTerrainGroup = GameObject.Find("地形群組").transform;
+
+        randomTerrain = Random.Range(1, 10000);                 //取得隨機地形數值
+
+        Generate();
+        GeneratePlayer();
     }
     #endregion
 
+    [Header("泥土範圍")]
+    public Vector2 v2Dirty = new Vector2(1, 6);
+
     #region 方法
+    /// <summary>
+    /// 生成地形
+    /// </summary>
     private void Generate()
     {
+        // 取得陣列資料語法 : 陣列名稱[標號]
+        for (int posX = 0; posX < x; posX++)
+        {
+            for (int posZ = 0; posZ < z; posZ++)
+            {
+                // 服點數轉整數 - (int)浮點數
+                int posY = (int)(Mathf.PerlinNoise(posX / detial + randomTerrain, posZ / detial + randomTerrain) * height);
 
+                Vector3 pos = new Vector3(posX, posY * heightTerrain, posZ);
+                Instantiate(objTerrains[0], pos, Quaternion.identity, traTerrainGroup);                     // 生成(物件,座標,角度,父物件)
+
+                #region 第一層底下的地形物件處理 : 泥土與石頭
+                for (int y = 0; y < posY; y++)                                                              // 第一層底下 從高度 0 開始
+                {
+                    int rDirty = (int)Random.Range(v2Dirty.x, v2Dirty.y);                                   //隨機泥土
+
+                    if (y >= posY - rDirty)                                                                 //第一層下方先顯示泥土在顯示石頭
+                    {
+                        Vector3 posDirty = new Vector3(posX, y * heightTerrain, posZ);
+                        Instantiate(objTerrains[1], posDirty, Quaternion.identity, traTerrainGroup);
+                    }
+                    else
+                    {
+                        Vector3 posStone = new Vector3(posX, y * heightTerrain, posZ);
+                        Instantiate(objTerrains[2], posStone, Quaternion.identity, traTerrainGroup);
+                    }
+                }
+                #endregion
+            }
+
+        }
+
+
+    }
+
+    private void GeneratePlayer()
+    {
+        Vector3 pos = new Vector3(x / 2, height + 3, z / 2);
+        Instantiate(traPlayer, pos, Quaternion.identity);
     }
     #endregion
 }
