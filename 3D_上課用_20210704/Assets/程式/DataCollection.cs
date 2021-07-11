0c6e877b39a42cffd0a8238b6eb0b50c8387f448
lazysealy <82298507+lazysealy@users.noreply.github.com>
2021-07-11 17:55:48 +0800

    0711全進度完

---
+++ 3D_上課用_20210704/Assets/程式/DataCollection.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+// Scriptable Object 腳本化物件：將腳本以物件方式儲存於 Project 內
+// 需要搭配類別屬性 CAM 建立資料選單
+// fileName 檔案名稱,menuName 選單名稱 - 可利用 / 設定階層
+/// <summary>
+/// 採集物件資料
+/// </summary>
+[CreateAssetMenu(fileName = "採集物件資料", menuName = "作業/採集物件資料")]
+
+public class DataCollection : ScriptableObject
+{
+    [Header("採集物件血量"), Range(0, 100)]
+    public float hp;
+    [Header("掉落物")]
+    public GameObject objDrop;
+}
--- 3D_上課用_20210704/Assets/程式/Player.cs
+++ 3D_上課用_20210704/Assets/程式/Player.cs
@@ -3,6 +3,10 @@
 public class Player : MonoBehaviour
 {
     #region 欄位
+    [Header("採集範圍"), Range(0, 30)]
+    public float rangeCollection = 2.5f;
+
+
     private Rigidbody rig;
     private Animator ani;
     private Transform traCamera;
@@ -17,8 +21,38 @@ public class Player : MonoBehaviour
         ani = transform.Find("男生").GetComponent<Animator>();
         traCamera = GameObject.Find("攝影機").transform;
     }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        // 圖示.繪製射線(中心點，射線方向)
+        Gizmos.DrawRay(traCamera.position, traCamera.forward * rangeCollection);
+    }
+
+    private void Update()
+    {
+        Collection();
+    }
     #endregion
 
     #region 方法
+
+    /// <summary>
+    /// 採集：繪製射線並判定是否攻擊以及採集物件
+    /// </summary>
+    private void Collection()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            // 射線碰撞資訊
+            RaycastHit hit;
+            //物理.射線碰撞(中心點.射線方向,射線碰撞資訊 射線長度.圖層) - out 將資訊儲存再輸入的欄位內
+            if (Physics.Raycast(traCamera.position, traCamera.forward, out hit, rangeCollection, 1 << 8))
+            {
+                // 對採集物件造成傷害
+                hit.collider.GetComponent<ObjectCollection>().Hit(1);
+            }
+        }
+    }
     #endregion
 }
