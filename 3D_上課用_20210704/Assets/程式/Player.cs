79882fba6fa7042c58fa326d0c80443512509f5f
lazysealy <82298507+lazysealy@users.noreply.github.com>
2021-07-11 13:00:44 +0800

    0711上午進度完成

---
+++ 3D_上課用_20210704/Assets/程式/Player.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    #region 欄位
+    private Rigidbody rig;
+    private Animator ani;
+    private Transform traCamera;
+    #endregion
+
+    #region 事件
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+
+        // transform.Find("子物件名稱") 透過名稱搜尋子物件
+        ani = transform.Find("男生").GetComponent<Animator>();
+        traCamera = GameObject.Find("攝影機").transform;
+    }
+    #endregion
+
+    #region 方法
+    #endregion
+}
